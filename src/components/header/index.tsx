3810f7bd5ab80ffcfb8a0e23f43e70208939276d
AmandaCleto <amanda.clet@gmail.com>
2019-12-16 20:21:21 -0300

    adds changes on the login page

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,15 +1,16 @@
 import React, { PureComponent } from 'react';
 import {Link} from 'react-router-dom';
 import './style.css';
- 
+
 class Header extends PureComponent { 
     public render(): React.ReactNode {
         return ( 
         <header>
            <h1>Tik Tak Toe - Master</h1> 
+           <h2 className="symbol">#</h2>
             <div className="button">
-                <Link to='/login'>Login</Link>
-                <Link to='/register'>Register</Link>
+                <Link to='/login' className="login">Login</Link>
+                <Link to='/register' className="register">Register</Link>
             </div>
         </header>  
         );       
