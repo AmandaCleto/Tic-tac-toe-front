1f83f13f414c053ccb98c8521ac861256f1383b3
matheusrajeduzz <matheus.mendes@eduzz.com>
2020-01-31 20:41:40 -0300

    creates lobby_row component

--- src/pages/public/components/showCase/components/LobbyMode/index.tsx
+++ src/pages/public/components/showCase/components/LobbyMode/index.tsx
@@ -1,27 +1,28 @@
 import React, { PureComponent } from 'react';
 import LobbyBoard from './components/LobbyBoard';
 import './style.css';
-//import Hidden from '@material-ui/core/Hidden';
-//import Button from '@material-ui/core/Button';
+import Hidden from '@material-ui/core/Hidden';
+import Button from '@material-ui/core/Button';
 
 export default class LobbyMode extends PureComponent<{},{}> {
     render() {
 
         return (
-
             <div className="lobby_mode">
-                <div className="lobby_arrow --previous" />
-                <LobbyBoard />
-                <div className="lobby_arrow --next" />
-            </div>
+                <h3>VS IA</h3>
 
-            /*
-            <Hidden smUp>
-            <Button className="lobby_button" variant="contained" color="primary">
-                Jogar
-            </Button> 
-            </Hidden>
-            */
+                <div className="lobby_row">
+                    <div className="lobby_arrow --previous" />
+                    <LobbyBoard />
+                    <div className="lobby_arrow --next" />
+                </div>
+
+                <Hidden smUp>
+                    <Button className="lobby_button" variant="contained" color="primary">
+                        Jogar
+                    </Button> 
+                </Hidden>
+            </div>
         );
     }
 }
